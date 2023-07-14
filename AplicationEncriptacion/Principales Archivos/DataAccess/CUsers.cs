1776aab67e59b0b3f6708cb49aa3556bee508d7f
Bryan Sebastián Yanzapanta Ontaneda <bsyanzapanta@espe.edu.ec>
2023-07-13 20:01:49 -0500

    final

---
+++ AplicationEncriptacion/Principales Archivos/DataAccess/CUsers.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DataAccess
+{
+    public class CUsers
+    {
+        Conexion cnn = new Conexion();
+        SqlConnection sqc = new SqlConnection();
+
+        public static SqlConnection conectar()
+        {
+
+            SqlConnection cn = new SqlConnection("SERVER=SEBAS-PC\\SQLEXPRESS; DATABASE=tramites; integrated security=true; ");
+            cn.Open();
+            return cn;
+
+        }
+
+        public bool validarUser(string username, string password)
+        {
+            bool esValido = false;
+
+            conectar();
+
+            string hashedPassword = HashPassword(password);
+
+            string sql = "SELECT * FROM Users WHERE username=@username AND password=@password";
+
+            using (SqlCommand command = new SqlCommand(sql, conectar()))
+            {
+                command.Parameters.AddWithValue("@username", username);
+                command.Parameters.AddWithValue("@password", hashedPassword);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        // Usuario válido
+                        esValido = true;
+                    }
+                    else
+                    {
+                        // Usuario inválido
+                        esValido = false;
+                    }
+                }
+            }
+
+            return esValido;
+        }
+
+
+        public bool verificarExistenciaUsuario(string username)
+        {
+            conectar();
+
+            string sql = "SELECT COUNT(*) FROM Users WHERE username = @username";
+
+            using (SqlCommand command = new SqlCommand(sql, conectar()))
+            {
+                command.Parameters.AddWithValue("@username", username);
+
+                int usuarioExistente = (int)command.ExecuteScalar();
+
+                return usuarioExistente > 0;
+            }
+        }
+
+        public bool registrarUsuario(string username, string password, string email)
+        {
+            bool exito = false;
+
+            conectar();
+
+            if (verificarExistenciaUsuario(username))
+            {
+                // El usuario ya existe, no se puede registrar nuevamente
+                
+                return false;
+            }
+
+
+            // Encriptar la contraseña
+            string hashedPassword = HashPassword(password);
+
+            string sql = "INSERT INTO Users (username, password, email) VALUES (@username, @password, @email)";
+
+            using (SqlCommand command = new SqlCommand(sql, conectar()))
+            {
+                command.Parameters.AddWithValue("@username", username);
+                command.Parameters.AddWithValue("@password", hashedPassword);
+                command.Parameters.AddWithValue("@email", email);
+
+                int rowsAffected = command.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    // Registro exitoso
+                    exito = true;
+                }
+                else
+                {
+                    // Error al registrar
+                    exito = false;
+                }
+            }
+
+            return exito;
+        }
+
+        private string HashPassword(string password)
+        {
+            using (SHA256Managed sha256 = new SHA256Managed())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(password);
+                byte[] hash = sha256.ComputeHash(bytes);
+                return Convert.ToBase64String(hash);
+            }
+        }
+
+
+    }
+}
---
+++ AplicationEncriptacion/Principales Archivos/WTramites/PrincipalLogin.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PrincipalLogin.aspx.cs" Inherits="WTramites.PrincipalLogin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 112px;
+        }
+        .auto-style2 {
+            width: 112px;
+            height: 23px;
+        }
+        .auto-style3 {
+            height: 23px;
+        }
+        .auto-style4 {
+            width: 136px;
+        }
+        .auto-style5 {
+            height: 23px;
+            width: 136px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table style="width:100%;">
+                <tr>
+                    <td class="auto-style1">Usuario</td>
+                    <td class="auto-style4">
+                        <input id="Hidden1" type="hidden" />
+                        <asp:TextBox ID="TxtUsername" runat="server"></asp:TextBox>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Contraseña</td>
+                    <td class="auto-style5">
+                        <asp:TextBox ID="TxtPassword" runat="server" TextMode="Password"></asp:TextBox>
+                    </td>
+                    <td class="auto-style3"></td>
+                </tr>
+                <tr>
+                    <td class="auto-style1">&nbsp;</td>
+                    <td class="auto-style4">
+                        <asp:Button ID="BtnLogin" runat="server" OnClick="BtnLogin_Click" Text="Login" />
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style4">
+                        <asp:Button ID="BtnRegister" runat="server"  Text="Register" OnClick="BtnRegister_Click" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
